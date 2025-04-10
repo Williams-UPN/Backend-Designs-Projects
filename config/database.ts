a68be3c410ab15efe9556634b5869295e6c9aa30
Williams <williams.j.xxi@gmail.com>
2025-04-10 18:00:00 -0500

    env

--- config/database.ts
+++ config/database.ts
@@ -20,7 +20,7 @@ export default ({ env }) => {
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
-      },
+      }, 
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10),
